d107702677d873443750fd98e7fcd74e5bb06856
satvik <satvik@satviks-MacBook-Air.local>
2025-09-05 01:37:46 +0530

    Add program and GitHub Actions workflow

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,8 @@
-# app.py
-# This is a test commit
+# add of 2 nums.
 def add(a, b):
     return a + b
 
+# unit testing of add f
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
